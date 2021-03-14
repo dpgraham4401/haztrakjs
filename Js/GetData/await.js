3140c876e58163fb79bf3c4d4b87e94566b62699
dpgraham4401 <dpgraham4401@gmail.com>
2021-03-14 16:16:22 -0400

    add axios library

---
+++ Js/GetData/await.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+let baseUrl = 'https://www.metaweather.com/api/location/';
+
+async function getID() {
+    let response = await axios.get('https://www.metaweather.com/api/location/search/?query=dc');
+    let data = response.data[0];
+    return data.woeid
+}
+
+async function getWet() {
+    let dcWoe = await getID();
+    console.log("This data comes from MetaWeather.com's API");
+    console.log('getting weather for DC (',dcWoe,')');
+
+    let getWetUrl = baseUrl + dcWoe;
+    //console.log(getWetUrl);
+
+    let response = await axios.get(getWetUrl);
+    let data = response.data.consolidated_weather[5];
+
+    console.log("the current wind speed is: " + data.wind_speed + " mph");
+
+    
+}
+
+getWet()
