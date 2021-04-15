0ab73b215e48e8219882d09cbab312701ad461d7
dpgraham4401 <dpgraham4401@gmail.com>
2021-04-14 22:30:23 -0400

    saving work, server down

--- Js/Services/linkServices.js
+++ Js/Services/linkServices.js
@@ -41,8 +41,64 @@ async function dashLink(dash = 'Dashboard', siteID){
     }
 }
 
+
+async function eManLink(page = 'Dashboard', siteID, mtn){
+    try{
+        if (page.length > 5){
+            if (page.toUpperCase() == 'BULLKSIGN'){
+                page = 'BulkSign';
+            }
+            else {
+                page = 'Dashboard';
+            }
+            const resUiLink = await eManAPI.post( {
+                url: `/links/emanifest`,
+                data: {
+                    "page": page,
+                    "epaSiteId": siteID
+                }
+            })
+        }
+        // else if (page.toUpperCase() == 'DASHBOARD'){
+        //     const resUiLink = await eManAPI.post( {
+        //         url: `/links/emanifest`,
+        //         data: {
+        //             "page": page,
+        //             "epaSiteId": siteID
+        //         }
+        //     })
+        // }
+        else {
+            page = page.charAt(0).toUpperCase() + page.slice(1).toLowerCase()
+            mtn = mtn.toUpperCase();
+            siteID = siteID.toUpperCase()
+                const resUiLink = await eManAPI.post( {
+                    url: `/links/emanifest`,
+                    data: {
+                       "page": page,
+                       "epaSiteId": siteID,
+                       "manifestTrackingNumber": mtn
+                    }
+                })
+            console.log(resUiLink.data);
+           }
+        // siteID = siteID.toUpperCase()
+        //     const resUiLink = await eManAPI.post( {
+        //         url: `/links/emanifest`,
+        //         data: {
+        //            "page": page,
+        //            "epaSiteId": siteID
+        //         }
+        //     })
+        // console.log(resUiLink.data);
+        } 
+    catch (error) {
+        console.error(error);
+    }
+}
+
 // Testing Area
-const dash = 'Dashboard';
+const page = 'View';
 const siteID  = 'VATEST000001';
-// const mtn = '100031335ELc';
-const siteDate = dashLink(dash, siteID)
+const mtn = '100031335ELC';
+const siteDate = mtnLink(page, siteID, mtn)
