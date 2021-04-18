3afc8098eb56b6e473937bbd08c43475a5768ce1
dpgraham4401 <dpgraham4401@gmail.com>
2021-04-18 18:42:26 -0400

    redo for new branch

---
+++ Js/Services/eManServices.js
@@ -0,0 +1,17 @@
+// e-Manifest Services
+const eManAPI = require('./eManAPI');
+async function eManGet(mtn) {
+    try{
+        const res = await eManAPI.get({
+            url: `/emanifest/manifest/${mtn}`
+        })
+        console.log(res.data);
+    }
+    catch (error){
+        console.error(error);
+    }
+}
+
+// Testing Area
+const mtn = '014059555JJK';
+const test = eManGet(mtn);
