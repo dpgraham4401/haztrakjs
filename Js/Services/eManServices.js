b7b2643676b8b6323554f01eccaef6e60ba509ba
dpgraham4401 <dpgraham4401@gmail.com>
2021-04-25 14:36:27 -0400

    add parse-multipart module

--- Js/Services/eManServices.js
+++ Js/Services/eManServices.js
@@ -1,16 +1,31 @@
 // e-Manifest Services
 const eManAPI = require('./eManAPI');
 const fs = require('fs');
+const multipart = require('parse-multipart');
 
 async function eManGet(mtn, attachments=0) {
     try{
         if (attachments){
             var res = await eManAPI.get({
                 'url': `/emanifest/manifest/${mtn}/attachments`,
-                'headers':{
-                    'Accept': 'multipart/mixed'
+                responeType: 'multipart/form-data',
+                headers:{
+                    Accept: 'multipart/mixed'
                 }
             })
+            var boundIndex = res.headers['content-type'].indexOf('boundary=') + 'boundary='.length;
+            var bound = res.headers['content-type'].slice(boundIndex, res.headers['content-type'].length);
+
+            var parts = multipart.Parse(res.data,bound);
+            for (let i=0; i<parts.length;i++){
+                
+            }
+
+            // fs.writeFile('test.zip', res.data, function (err) {
+            //     if (err) {
+            //         return console.error(err);
+            //     }
+            // });
         }
         else {
             var res = await eManAPI.get({
@@ -20,7 +35,7 @@ async function eManGet(mtn, attachments=0) {
                 }
             })
         }
-       console.log(res);
+       console.log(bound);
     }
     catch (error){
         console.error(error);
@@ -30,4 +45,4 @@ async function eManGet(mtn, attachments=0) {
 // Testing Area
 const mtn = '100024721ELC';
 const attachments = 1;
-const test = eManGet(mtn, attachments);
+eManGet(mtn, attachments);
