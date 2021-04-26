4fbeed380215604a720bac1b5d37c515b2bcd220
dpgraham4401 <dpgraham4401@gmail.com>
2021-04-25 21:44:15 -0400

    JSON successful retrieve, zip still buggy

--- Js/Services/eManServices.js
+++ Js/Services/eManServices.js
@@ -1,31 +1,30 @@
 // e-Manifest Services
 const eManAPI = require('./eManAPI');
 const fs = require('fs');
-const multipart = require('parse-multipart');
 
 async function eManGet(mtn, attachments=0) {
     try{
         if (attachments){
             var res = await eManAPI.get({
                 'url': `/emanifest/manifest/${mtn}/attachments`,
-                responeType: 'multipart/form-data',
+                responeType: 'blob',
                 headers:{
                     Accept: 'multipart/mixed'
                 }
             })
             var boundIndex = res.headers['content-type'].indexOf('boundary=') + 'boundary='.length;
-            var bound = res.headers['content-type'].slice(boundIndex, res.headers['content-type'].length);
+            var bound = '--' + res.headers['content-type'].slice(boundIndex, res.headers['content-type'].length);
+            var emanData = res.data.split(bound);
 
-            var parts = multipart.Parse(res.data,bound);
-            for (let i=0; i<parts.length;i++){
-                
-            }
+            fs.writeFile('./test.json', emanData[1], function (err) {
+                if (err) {
+                    return console.log(err);
+                }});
+                fs.writeFile('./attachment_test.zip', emanData[2],'binary', function (err) {
+                    if (err) {
+                        return console.log(err);
+                }});
 
-            // fs.writeFile('test.zip', res.data, function (err) {
-            //     if (err) {
-            //         return console.error(err);
-            //     }
-            // });
         }
         else {
             var res = await eManAPI.get({
@@ -35,7 +34,9 @@ async function eManGet(mtn, attachments=0) {
                 }
             })
         }
-       console.log(bound);
+    //    console.log(typeof(res.data));
+    //    console.log(bound);
+    //    console.log(emanData[2]);
     }
     catch (error){
         console.error(error);
