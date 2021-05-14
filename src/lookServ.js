0135d541376d4ccc0ef4696d9d3b2a7d60286dcd
dpgraham4401 <dpgraham4401@gmail.com>
2021-05-14 17:00:16 -0400

    feat: rcrainfo lookup

--- src/lookServ.js
+++ src/lookServ.js
@@ -7,16 +7,33 @@ import * as eManAPI from './eManAPI.js'
  *
  * @param {string} den, min, port, form, source, state, fed, or mngt
  * */
-async function lookup (codes) {
+async function lookup (codes, stCode = 'CA') {
   try {
     let codeUrl = ''
+    // if (typeof (codes) !== 'string') {
+    //  throw new Error('code type must be string')
+    // }
     if (codes === 'den') {
       codeUrl = '/lookup/density-uom'
+    } else if (codes === 'form') {
+      codeUrl = '/lookup/form-codes'
+    } else if (codes === 'source') {
+      codeUrl = '/lookup/source-codes'
+    } else if (codes === 'state') {
+      codeUrl = `/lookup/state-waste-codes/${stCode}`
+    } else if (codes === 'fed') {
+      codeUrl = 'lookup/federal-waste-codes'
+    } else if (codes === 'min') {
+      codeUrl = 'lookup/waste-minimization-codes'
+    } else if (codes === 'ports') {
+      codeUrl = 'lookup/ports-of-entry'
+    } else {
+      throw new Error('unknown code url')
     }
     const res = await eManAPI.get({
       url: codeUrl
     })
-    // console.log(res.data)
+    console.log(res.data)
     return res.data
   } catch (error) {
     console.error(error)
@@ -26,4 +43,4 @@ async function lookup (codes) {
 export { lookup }
 
 // test area
-lookup('den')
+lookup('fed')
