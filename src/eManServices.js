5ac05dc5092a31a77a9d954ab9605c0a4167fd03
dpgraham4401 <dpgraham4401@gmail.com>
2021-05-25 22:08:05 -0400

    refactor: disable attachemtn for now

--- src/eManServices.js
+++ src/eManServices.js
@@ -12,37 +12,15 @@ import * as eManAPI from './eManAPI.js'
 async function eManGet (mtn, attachments = 0) {
   try {
     if (attachments) {
-      const res = await eManAPI.get({
-        url: `/emanifest/manifest/${mtn}/attachments`,
-        // responeType: 'blob',
-        headers: {
-          Accept: 'multipart/mixed'
-        }
-      })
-      // var boundIndex = res.headers['content-type'].indexOf('boundary=') + 'boundary='.length
-      // var bound = '--' + res.headers['content-type'].slice(boundIndex, res.headers['content-type'].length)
-
-      // var d = new Dicer({boundary: bound})
-      // d.on('part', function(p) {
-      //     console.log('New Part!');
-      //     p.on('header', function(header){
-      //         for (var h in header) {
-      //             console.log('Part header: K ' + inspect(h) + ' ' inspect(header[h]));
-      //         }
-      //     })
-      // })
-
-      // fs.writeFile('./test.json', emanData[1], function (err) {
-      //   if (err) {
-      //     return console.log(err)
+      // const res = await eManAPI.get({
+      //   url: `/emanifest/manifest/${mtn}/attachments`,
+      //   // responeType: 'blob',
+      //   headers: {
+      //     Accept: 'multipart/mixed'
       //   }
       // })
-      // fs.writeFile('./attachment_test.zip', emanData[2], 'binary', function (err) {
-      //   if (err) {
-      //     return console.log(err)
-      //   }
-      // })
-      return res
+      // return res
+      console.log('This feature is unsupported for this release')
     } else {
       const res = await eManAPI.get({
         url: `/emanifest/manifest/${mtn}`,
