7d05b0051157dc0cc8b79b11acbffb2ba0b18700
David Paul Graham <dpgraham4401@gmail.com>
2021-06-30 22:29:06 -0400

    fix: alpha phase eManifest save

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { siteExist, siteDetails } from './siteServices.js'
 import { eManLink } from './linkServices.js'
-import { eManGet } from './eManServices.js'
+import * as eMan from './eManServices.js'
 import { lookup } from './lookServ.js'
 import { eManLookup } from './eManLookup.js'
 
 export { siteExist }
 export { siteDetails }
 export { eManLink }
-export { eManGet }
+export { eMan }
 export { lookup }
 export { eManLookup }
