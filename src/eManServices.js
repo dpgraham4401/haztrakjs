59004a1240dabfcc2a19cada8c9cd0a43020d102
David Paul Graham <dpgraham4401@gmail.com>
2021-07-09 07:58:53 -0400

    fix: eMan save attachments

--- src/eManServices.js
+++ src/eManServices.js
@@ -1,6 +1,7 @@
 // e-Manifest Services
 import FormData from 'form-data'
 import * as eManAPI from './eManAPI.js'
+import fs from 'fs'
 
 /**
  * Get manifests Data and/or attachment
@@ -12,8 +13,15 @@ async function eManGet (mtn, attachments = false) {
   try {
     mtn = mtn.toUpperCase()
     if (attachments) {
-      // getting attachments not suppported yet
-      console.log('This feature is unsupported for this release')
+      const res = await eManAPI.get({
+        url: `/emanifest/manifest/${mtn}/attachments`,
+        headers: {
+          Accept: 'multipart/mixed'
+        }
+      })
+      const form = new FormData()
+      form.append('data', res.data)
+      return res.data
     } else {
       const res = await eManAPI.get({
         url: `/emanifest/manifest/${mtn}`,
@@ -57,13 +65,25 @@ async function eManDel (mtn) {
   * Save manifest with JSON and optional zip attachment
   *
   * @param {string} mtnJson manifest object
-  * @param {string} path path to zip attachment
+  * @param {string} zipPath Path to zip attachment
   * */
-async function eManSave (mtnJson, path) {
-  if (path) {
-    // submitting zip attachment not supported in this version
-  } else {
-    try {
+async function eManSave (mtnJson, zipPath) {
+  try {
+    if (zipPath) {
+      const form = new FormData()
+      form.append('manifest', mtnJson)
+      form.append('attachment', fs.createReadStream(zipPath))
+      const formHeaders = form.getHeaders()
+      console.log(formHeaders)
+      const res = await eManAPI.post({
+        url: 'emanifest/manifest/save',
+        headers: {
+          ...formHeaders
+        },
+        data: form
+      })
+      return res.data
+    } else {
       const form = new FormData()
       form.append('manifest', mtnJson)
       const formHeaders = form.getHeaders()
@@ -75,11 +95,11 @@ async function eManSave (mtnJson, path) {
         data: form
       })
       return res.data
-    } catch (error) {
-      console.error('Problem saving manifest')
-      console.error(error.message)
-      console.error(error.response.data)
     }
+  } catch (error) {
+    console.error('Problem saving manifest')
+    console.error(error.message)
+    console.error(error.response.data)
   }
 }
 
