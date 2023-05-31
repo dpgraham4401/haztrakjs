a0db02ad25cb283a7d72881485d0bd051abd4722
David Paul Graham <dpgraham4401@gmail.com>
2023-05-31 17:04:54 -0400

    Add services as RcraClient methods

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,18 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
-import { AuthResponse, PackingGroups } from './types';
+import {
+  AuthResponse,
+  BillGetParameters,
+  BillHistoryParameters,
+  BillSearchParameters,
+  ManifestCorrectionParameters,
+  ManifestExistsResponse,
+  ManifestSearchParameters,
+  PackingGroups,
+  QuickerSign,
+  RcraCode,
+  SiteSearchParameters,
+  UserSearchParameters,
+} from './types';
 
 export const RCRAINFO_PREPROD = 'https://rcrainfopreprod.epa.gov/rcrainfo/rest/api';
 export const RCRAINFO_PROD = 'https://rcrainfo.epa.gov/rcrainfo/rest/api';
@@ -84,12 +97,53 @@ class RcraClient {
     return this.token !== undefined;
   };
 
-  public getSite = async (siteID: string): Promise<AxiosResponse> => {
-    return this.apiClient.get(`v1/site-details/${siteID}`);
+  // RCRAInfo Lookup Services
+
+  public getStateWasteCodes = async (stateCode: string): Promise<AxiosResponse<RcraCode[]>> => {
+    return this.apiClient.get(`v1/lookup/state-waste-codes/${stateCode}`);
   };
 
-  public getHazardClasses = async (shippingName?: string, idNumber?: string): Promise<AxiosResponse> => {
-    if (shippingName && idNumber) {
+  public getFederalWasteCodes = async (): Promise<AxiosResponse<RcraCode[]>> => {
+    return this.apiClient.get('v1/lookup/federal-waste-codes');
+  };
+
+  public getDensityUOMs = async (): Promise<AxiosResponse<RcraCode[]>> => {
+    return this.apiClient.get('v1/lookup/density-uom');
+  };
+
+  public getSourceCodes = async (): Promise<AxiosResponse<RcraCode[]>> => {
+    return this.apiClient.get('v1/lookup/source-codes');
+  };
+
+  public getManagementMethodCodes = async (): Promise<AxiosResponse<RcraCode[]>> => {
+    return this.apiClient.get('v1/lookup/management-method-codes');
+  };
+
+  public getWasteMinimizationCodes = async (): Promise<AxiosResponse<RcraCode[]>> => {
+    return this.apiClient.get('v1/lookup/waste-minimization-codes');
+  };
+
+  public getPortsOfEntry = async (): Promise<AxiosResponse<RcraCode[]>> => {
+    return this.apiClient.get('v1/lookup/ports-of-entry');
+  };
+
+  // e-Manifest Lookup Services
+
+  /**
+   * Returns a list of all available DOT Hazard classes.
+   * @param shippingName
+   * @param idNumber
+   */
+  public getHazardClasses = async (
+    shippingName?: string,
+    idNumber?: string,
+  ): Promise<AxiosResponse<string[] | string>> => {
+    if (shippingName || idNumber) {
+      // if either shippingName or idNumber is provided, attempt to get by shipping name and id number
+      if (!shippingName || !idNumber) {
+        // if only one is provided, throw an error
+        throw new Error('Please provide both a shipping name and an ID number.');
+      }
       return this.apiClient.get(
         `v1/emanifest/lookup/hazard-classes-by-shipping-name-id-number/${shippingName}/${idNumber}`,
       );
@@ -97,7 +151,122 @@ class RcraClient {
     return this.apiClient.get('v1/emanifest/lookup/hazard-classes');
   };
 
-  public getPackingGroups = async (): Promise<AxiosResponse<PackingGroups>> => {
+  public getPackingGroups = async (
+    shippingName?: string,
+    idNumber?: string,
+  ): Promise<AxiosResponse<string[] | string>> => {
+    if (shippingName || idNumber) {
+      // if either shippingName or idNumber is provided, attempt to get by shipping name and id number
+      if (!shippingName || !idNumber) {
+        // if only one is provided, throw an error
+        throw new Error('Please provide both a shipping name and an ID number.');
+      }
+      return this.apiClient.get(
+        `v1/emanifest/lookup/packing-groups-by-shipping-name-id-number/${shippingName}/${idNumber}`,
+      );
+    }
     return this.apiClient.get('v1/emanifest/lookup/packing-groups');
   };
+
+  // Site Services
+
+  public getSite = async (siteID: string): Promise<AxiosResponse> => {
+    return this.apiClient.get(`v1/site-details/${siteID}`);
+  };
+
+  public getSiteExists = async (siteID: string): Promise<AxiosResponse<{ result: boolean; epaSiteId: string }>> => {
+    return this.apiClient.get(`v1/site-exists/${siteID}`);
+  };
+
+  public searchSites = async (searchParameters: SiteSearchParameters): Promise<AxiosResponse<any>> => {
+    return this.apiClient.post('v1/site-search', searchParameters);
+  };
+
+  // User Services
+
+  public searchUsers = async (searchParameters: UserSearchParameters): Promise<AxiosResponse<any>> => {
+    return this.apiClient.post('v1/user/user-search', searchParameters);
+  };
+
+  // e-Manifest Services
+
+  public getBill = async (searchParameters: BillGetParameters): Promise<AxiosResponse<any>> => {
+    return this.apiClient.post('v1/emanifest/billing/bill', searchParameters);
+  };
+
+  public searchBill = async (searchParameters: BillSearchParameters): Promise<AxiosResponse<any>> => {
+    return this.apiClient.post('v1/emanifest/billing/bill-search', searchParameters);
+  };
+
+  public getBillHistory = async (searchParameters: BillHistoryParameters): Promise<AxiosResponse<any>> => {
+    return this.apiClient.post('v1/emanifest/billing/bill-history', searchParameters);
+  };
+
+  // ToDo
+  // public updateManifest = async (): Promise<AxiosResponse<any>> => {
+  //   return this.apiClient.put('v1/emanifest/manifest/update');
+  // };
+
+  public deleteManifest = async (manifestTrackingNumber: string): Promise<AxiosResponse<any>> => {
+    return this.apiClient.delete(`v1/emanifest/manifest/delete${manifestTrackingNumber}`);
+  };
+
+  // ToDo
+  // public saveManifest = async (): Promise<AxiosResponse<any>> => {
+  //   return this.apiClient.post('v1/emanifest/manifest/save');
+  // };
+
+  // ToDo
+  // public getManifestAttachments = async (manifestTrackingNumber: string): Promise<AxiosResponse<any>> => {
+  //   return this.apiClient.get(`v1/emanifest/manifest/${manifestTrackingNumber}/attachments`);
+  // };
+
+  public getManifestCorrections = async (manifestTrackingNumber: string): Promise<AxiosResponse<any>> => {
+    return this.apiClient.get(`v1/emanifest/manifest/correction-details/${manifestTrackingNumber}`);
+  };
+
+  public getManifestCorrectionVersion = async (
+    parameters: ManifestCorrectionParameters,
+  ): Promise<AxiosResponse<any>> => {
+    return this.apiClient.post('v1/emanifest/manifest/correction-version', parameters);
+  };
+
+  public getManifestCorrectionAttachments = async (
+    parameters: ManifestCorrectionParameters,
+  ): Promise<AxiosResponse<any>> => {
+    return this.apiClient.post('v1/emanifest/manifest/correction-version/attachments', parameters);
+  };
+
+  public getMTN = async (siteID: string): Promise<AxiosResponse<any>> => {
+    return this.apiClient.get(`v1/emanifest/manifest-tracking-numbers/${siteID}`);
+  };
+
+  public getSiteID = async (stateCode: string, siteType: string): Promise<AxiosResponse<any>> => {
+    return this.apiClient.get(`v1/emanifest/site-ids/${stateCode}/${siteType}`);
+  };
+
+  public getManifest = async (manifestTrackingNumber: string): Promise<AxiosResponse<any>> => {
+    return this.apiClient.get(`v1/emanifest/manifest/${manifestTrackingNumber}`);
+  };
+
+  public searchManifest = async (parameters: ManifestSearchParameters): Promise<AxiosResponse<any>> => {
+    return this.apiClient.post('v1/emanifest/manifest/search', parameters);
+  };
+
+  public getMTNExists = async (manifestTrackingNumber: string): Promise<AxiosResponse<ManifestExistsResponse>> => {
+    return this.apiClient.get(`v1/emanifest/manifest/mtn-exists/${manifestTrackingNumber}`);
+  };
+
+  public revertManifest = async (manifestTrackingNumber: string): Promise<AxiosResponse<any>> => {
+    return this.apiClient.get(`v1/emanifest/manifest/revert/${manifestTrackingNumber}`);
+  };
+
+  public SignManifest = async (parameters: QuickerSign): Promise<AxiosResponse<any>> => {
+    return this.apiClient.post('v1/emanifest/manifest/quicker-sign', parameters);
+  };
+
+  // ToDo
+  // public correctManifest = async (): Promise<AxiosResponse<any>> => {
+  //   return this.apiClient.post('v1/emanifest/manifest/correct');
+  // };
 }
--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,94 @@ export interface AuthResponse {
 }
 
 export type PackingGroups = 'I' | 'II' | 'III';
+export type ManifestStatus =
+  | 'Pending'
+  | 'Scheduled'
+  | 'InTransit'
+  | 'Received'
+  | 'ReadyForSignature'
+  | 'Signed'
+  | 'SignedComplete'
+  | 'UnderCorrection'
+  | 'Corrected';
+
+export type SubmissionType = 'FullElectronic' | 'DataImage5Copy' | 'Hybrid' | 'Image';
+export type OriginType = 'Web' | 'Service' | 'Mail';
+
+export type SiteType = 'Generator' | 'Tsdf' | 'Transporter' | 'Rejection_AlternateTsdf';
+
+export interface RcraCode {
+  code: string;
+  description: string;
+}
+
+export interface SiteSearchParameters {
+  epaSiteId: string;
+  name: string;
+  streetNumber: string;
+  address1: string;
+  city: string;
+  state: string;
+  zip: string;
+  siteType: 'Generator' | 'Tsdf' | 'Transporter' | 'Broker';
+  pageNumber: number;
+}
+
+export interface UserSearchParameters {
+  userId: string;
+  siteIds: string[];
+  pageNumber: number;
+}
+
+export interface BillGetParameters {
+  billId: string;
+  billingAccount: string;
+  monthYear: string;
+}
+
+export interface BillHistoryParameters {
+  billingAccount: string;
+  startDate: string;
+  endDate: string;
+}
+
+export interface BillSearchParameters extends BillHistoryParameters {
+  billStatus: string;
+  amountChanged: boolean;
+  pageNumber: number;
+}
+
+export interface ManifestCorrectionParameters {
+  manifestTrackingNumber: string;
+  status: 'Signed' | 'Corrected' | 'UnderCorrection';
+  ppcStatus: 'PendingDataEntry' | 'DataQaCompleted';
+  version: number;
+}
+
+export interface ManifestSearchParameters {
+  stateCode: string;
+  siteId: string;
+  status: ManifestStatus;
+  dateType: 'CertifiedDate' | 'ReceivedDate' | 'ShippedDate' | 'UpdatedDate';
+  siteType: SiteType;
+  startDate: string;
+  endDate: string;
+  correctionRequestStatus: 'NotSend' | 'Sent' | 'industryResponded' | 'Cancelled';
+}
+
+export interface ManifestExistsResponse {
+  result: boolean;
+  submissionType: SubmissionType;
+  originType: OriginType;
+  manifestTrackingNumber: string;
+  status: ManifestStatus;
+}
+
+export interface QuickerSign {
+  manifestTrackingNumbers: string[];
+  siteId: string;
+  siteType: SiteType;
+  printedSignatureName: string;
+  printedSignatureDate: string;
+  transporterOrder: number;
+}
