a6710b93fbf445b0c24798b9ffec5654043e907a
David Graham <dpgraham4401@gmail.com>
2023-08-07 16:14:39 -0400

    Vite d-ts plugin to export type declarations

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,16 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'src/index.ts'),
-            name: 'emanifest-lib',
-            fileName: 'emanifest-lib',
-        },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'emanifest-lib',
+      fileName: 'emanifest-lib',
     },
-    plugins: [dts()],
+  },
+  plugins: [
+    dts({
+      rollupTypes: true,
+    }),
+  ],
 });
