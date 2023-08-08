1f3068621c2f88bd13f1b4240ac1c08cc6aba026
David Graham <dpgraham4401@gmail.com>
2023-08-08 08:47:57 -0400

    test showing authenticate method sets correct Authorization header for other service calls, add rollup configs

--- src/tests/client.spec.ts
+++ src/tests/client.spec.ts
@@ -11,30 +11,59 @@ afterAll(() => server.close());
 
 describe('RcraClient', () => {
   it('instantiates an object', () => {
-    const client = newClient({ apiBaseURL: RCRAINFO_PREPROD });
-    expect(typeof client).toBe('object');
-    expect(typeof client.authenticate).toBe('function');
+    const rcrainfo = newClient({ apiBaseURL: RCRAINFO_PREPROD });
+    expect(typeof rcrainfo).toBe('object');
+    expect(typeof rcrainfo.authenticate).toBe('function');
   });
   it('Retrieves a token when authenticating', async () => {
-    const client = newClient({ apiBaseURL: RCRAINFO_PREPROD, apiID: MOCK_API_ID, apiKey: MOCK_API_KEY });
-    await client.authenticate();
-    expect(client.token).toBe(MOCK_TOKEN);
+    const rcrainfo = newClient({ apiBaseURL: RCRAINFO_PREPROD, apiID: MOCK_API_ID, apiKey: MOCK_API_KEY });
+    await rcrainfo.authenticate();
+    expect(rcrainfo.token).toBe(MOCK_TOKEN);
   });
-  it('Auto-authentication is disabled by default', async () => {
-    const client = newClient({ apiBaseURL: RCRAINFO_PREPROD, apiID: MOCK_API_ID, apiKey: MOCK_API_KEY });
-    await client.getPackingGroups().catch((err: AxiosError) => {
+  it('does not enable auto-authentication by default', async () => {
+    const rcrainfo = newClient({ apiBaseURL: RCRAINFO_PREPROD, apiID: MOCK_API_ID, apiKey: MOCK_API_KEY });
+    // We expect a 401 because we didn't call authenticate() first
+    await rcrainfo.getPackingGroups().catch((err: AxiosError) => {
       expect(err.response?.status).toBe(401);
     });
   });
-  it('Auto-authentication is requests a token first', async () => {
-    const client = newClient({
+  it('auto-authenticates when enabled, and retrieved a token on calling any service', async () => {
+    const rcrainfo = newClient({
       apiBaseURL: RCRAINFO_PREPROD,
       apiID: MOCK_API_ID,
       apiKey: MOCK_API_KEY,
       authAuth: true,
     });
-    const resp: AxiosResponse = await client.getPackingGroups();
+    const resp: AxiosResponse = await rcrainfo.getPackingGroups();
+    expect(rcrainfo.token).toEqual(MOCK_TOKEN);
     expect(resp.data).toEqual(MOCK_PACKING_GROUPS);
+  });
+  it('adds a Authorization headers with "Bearer" + token', async () => {
+    const rcrainfo = newClient({
+      apiBaseURL: RCRAINFO_PREPROD,
+      apiID: MOCK_API_ID,
+      apiKey: MOCK_API_KEY,
+    });
+    rcrainfo
+      .authenticate()
+      .then((authResp: AxiosResponse) => {
+        expect(authResp.status).toBe(200);
+        expect(rcrainfo.token).toBeTruthy();
+        fail('should not have authenticated');
+        return rcrainfo.getPackingGroups();
+      })
+      .then((resp) => {
+        expect(resp.status).toBe(200);
+      })
+      .catch((err: AxiosError) => {
+        console.log('error', err);
+      });
+    // expect(authResponse.status).toBe(200);
+    // expect(rcrainfo.token).toBeTruthy(); // Ensure token is set
+    // console.log(rcrainfo.token);
+    // // Our mock service worker will return a 401 if Authorization: 'Bearer mockToken' is not set
+    // const resp = await rcrainfo.getPackingGroups();
+    // expect(resp.status).toBe(200);
   });
 });
 
--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,20 @@ import dts from 'vite-plugin-dts';
 // https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
   build: {
+    minify: false,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'emanifest-lib',
       fileName: 'emanifest-lib',
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['axios'],
+      output: {
+        globals: {
+          axios: 'axios',
+        },
+      },
     },
   },
   plugins: [
